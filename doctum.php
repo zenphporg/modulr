443deed471da6cfdd72c5246f576d433feed7a23
secondman <secondman@jetstreamlabs.com>
2024-06-02 06:26:36 -0700

    wip: update to just document src dir
    Signed-off-by: secondman <secondman@jetstreamlabs.com>

--- doctum.php
+++ doctum.php
@@ -5,7 +5,7 @@ use Doctum\RemoteRepository\GitLabRemoteRepository;
 use Doctum\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
-$dir = __DIR__;
+$dir = __DIR__.'/src';
 $iterator = Finder::create()
   ->files()
   ->name('*.php')
@@ -26,13 +26,5 @@ return new Doctum($iterator, [
   'cache_dir' => __DIR__.'/cache/%version%',
   'source_dir' => dirname($dir).'/',
   'remote_repository' => new GitLabRemoteRepository('zenphp/modulr', $dir),
-  'default_opened_level' => 1,
-  'footer_link' => [
-    'href' => 'https://gitlab.com/zenphp/modulr',
-    'rel' => 'noreferrer noopener',
-    'target' => '_blank',
-    'before_text' => 'You can edit the configuration',
-    'link_text' => 'on this', // Required if the href key is set
-    'after_text' => 'repository',
-  ],
+  'default_opened_level' => 2,
 ]);
